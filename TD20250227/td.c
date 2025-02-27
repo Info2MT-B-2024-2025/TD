f81c5baf17e2c567bed37515f587c006e91a0aaa
Pierre Bressy <pierre.bressy@heig-vd.ch>
2025-02-27 09:10:25 +0000

    added TD20250227

--- TD20250227/td.c
+++ TD20250227/td.c
@@ -2,59 +2,63 @@
 
 int main(int argc, const char *argv[])
 {
-	const char *filename="./data/list.txt"; // relatif
+	const char *filename = "./data/list.txt"; // relatif
 	// const char *filename="/workspaces/TD/TD20250227/data/list.txt"; // absolu
 	char *mode = "r"; // read the file
 
 	// open the file
-	FILE *f=NULL;
-	f=fopen(filename, mode);
-	if(NULL==f) {
+	FILE *f = NULL;
+	f = fopen(filename, mode);
+	if (NULL == f)
+	{
 		printf("Error, unable to open [%s].\n", filename);
 		return 1;
 	}
 	// read the file and count the number of characters (EOF excluded)
-	char c=' ';
-	size_t filesize=0;
+	char c = ' ';
+	size_t filesize = 0;
 
-	do {
-		c=fgetc(f);
-		if (c!=EOF) {
+	do
+	{
+		c = fgetc(f);
+		if (c != EOF)
+		{
 			printf("%c", c);
 			filesize++;
 		}
-	} while(c!=EOF);
-
+	} while (c != EOF);
 
 	printf("\nfilesize=%lu bytes.\n", filesize);
 
 	// close the file
-	if(0!=fclose(f)) {
+	if (0 != fclose(f))
+	{
 		printf("Error, unable to close [%s].\n", filename);
 		return 2;
 	};
 
-
 	// create ./data/ascii.txt
-	const char* output_filename="./data/ascii.txt";
-	mode= "w";
+	const char *output_filename = "./data/ascii.txt";
+	mode = "w";
 
-	f=fopen(output_filename,mode);
-	if(NULL==f) {
+	f = fopen(output_filename, mode);
+	if (NULL == f)
+	{
 		printf("Error, unable to open [%s].\n", output_filename);
 		return 1;
 	}
 
 	fprintf(f, "ASCII TABLE\n");
-
+	fprintf(f, "-----------\n");
+	for (int i = 32; i < 128; i++)
+		fprintf(f, "%5d %5c\n", i, i);
 
 	// close the file
-	if(0!=fclose(f)) {
+	if (0 != fclose(f))
+	{
 		printf("Error, unable to close [%s].\n", filename);
 		return 2;
 	};
-
-
 
 	return 0;
 }
