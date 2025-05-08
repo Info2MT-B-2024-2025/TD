b8a2c2dc599fbc00b3f2fa2d458d78bd553d317b
Pierre Bressy <pierre.bressy@heig-vd.ch>
2025-05-08 06:42:00 +0000

    test insertion

--- TD20250501/td.c
+++ TD20250501/td.c
@@ -7,17 +7,24 @@ int main(int argc, const char *argv[])
 	init_list(&l);
 	display_list(l);
 
-	elem e={.x=1, .y=1.};
+	elem e={.x=1, .y=2.};
 	err=insert_list(&l, e, 0);
 	if(err) printf("error=%d\n",err);
 	display_list(l);
 
-	e.x=2.;
-	e.y=2.;
-	err=insert_list(&l, e, 4);
+	e.x=5.;
+	e.y=6.;
+	err=insert_list(&l, e, 1);
 	if(err) printf("error=%d\n",err);
-
 	display_list(l);
+
+	e.x=3.;
+	e.y=4.;
+	err=insert_list(&l, e, 1);
+	if(err) printf("error=%d\n",err);
+	display_list(l);
+
+
 	return 0;
 }
 
